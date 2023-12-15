f46735f60ed4377e7e6b9cdfe747a1be4900796a
emmanuelmontoyalopez242006 <emmanuelprogramador24@gmail.com>
2023-12-14 21:12:03 -0500

    Practicando JavaScript

---
+++ JavaScript/ejercicioSeis.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Digite un numero"));
+if (numero % 5 === 0) {
+  alert(`El numero ${numero} es divisible por 5`);
+} else {
+  alert(`El numero ${numero} no es divisible por 5`);
+}
