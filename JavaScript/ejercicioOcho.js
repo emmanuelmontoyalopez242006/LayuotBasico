4a62619956fd7d406c87fa3d4339cd6bdfb26655
emmanuelmontoyalopez242006 <emmanuelprogramador24@gmail.com>
2023-12-14 21:16:58 -0500

    Practicando JavaScript

---
+++ JavaScript/ejercicioOcho.js
@@ -0,0 +1,7 @@
+let numeroUno = parseInt(prompt("Digite un numero"));
+let numeroDos = parseInt(prompt("Digite otro numero"));
+if (numeroUno > numeroDos) {
+  alert(`El numero mayor es el ${numeroUno}`);
+} else {
+  alert(`El numero mayor es el ${numeroDos}`);
+}
