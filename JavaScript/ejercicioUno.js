720b8c266e326608221770f9898fa7c73a3943f2
emmanuelmontoyalopez242006 <emmanuelprogramador24@gmail.com>
2023-12-14 20:50:07 -0500

    Practicando JavaScript

---
+++ JavaScript/ejercicioUno.js
@@ -0,0 +1,5 @@
+let numeroUno = parseInt(prompt("Digite un numero"));
+let numeroDos = parseInt(prompt("Digite un numero"));
+let sumarDosNumeros = numeroUno + numeroDos;
+
+alert(`La suma es ${sumarDosNumeros}`);
