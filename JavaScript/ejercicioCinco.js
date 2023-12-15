4e75bec8b159d60bdf108bbf55990211067c5a3f
emmanuelmontoyalopez242006 <emmanuelprogramador24@gmail.com>
2023-12-14 21:09:34 -0500

    Practicando JavaScript

---
+++ JavaScript/ejercicioCinco.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite un numero"));
+
+if (numero % 2 === 0) {
+  alert(`El numero ${numero} es par`);
+} else {
+  alert(`El numero ${numero} no es par`);
+}
---
+++ JavaScript/ejercicioCuatro.js
@@ -0,0 +1,3 @@
+let peticion = prompt("Digite un tipo de dato");
+
+alert(`El tipo de dato es ${typeof peticion}`);
